378e427ac4593a60a05b6870d91f347f2b8d0cc4
Jasper Kent <software@jasperkent.com>
2022-10-14 09:06:26 +0100

    Add project files.

---
+++ Prime.GUI/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prime.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource? _cancellationSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            _cancellationSource?.Cancel();
+        }
+
+        private async void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Cancel.IsEnabled = true;
+
+                _cancellationSource = new CancellationTokenSource();
+
+                Output.Items.Add("Calculating...");
+
+                var lo = ulong.Parse(Lo.Text, NumberStyles.AllowThousands);
+                var hi = ulong.Parse(Hi.Text, NumberStyles.AllowThousands);
+
+                Task<int>[] tasks =
+                {
+                    Task.Run(() => PrimeCount(lo, hi, _cancellationSource.Token)),
+                    Task.Run(() => PrimeCount(lo, hi, _cancellationSource.Token)),
+                    Task.Run(() => PrimeCount(lo, hi, _cancellationSource.Token))
+                };
+
+                var finisher = await Task.WhenAny(tasks);
+
+                _cancellationSource.Cancel();
+
+                Output.Items.Clear();
+             
+                ReportResult(finisher, tasks, lo, hi);
+            }
+            catch (Exception ex)
+            {
+                Output.Items.Clear();
+                Output.Items.Add(ex.Message);
+            }
+            finally
+            {
+                Cancel.IsEnabled = false;
+            }
+        }
+
+        private void ReportResult(Task<int> finisher, Task<int>[] tasks, ulong lo, ulong hi)
+        {
+            Output.Items.Add($"Task {Array.IndexOf(tasks, finisher)} reported {finisher.Result:N0} primes between {lo:N0}, and {hi:N0}");
+        }
+
+        private static int PrimeCount(ulong lo, ulong hi, CancellationToken token)
+        {
+            int count = 0;
+
+            var root = Math.Sqrt(hi);
+            var rand = new Random();
+
+            var primes = PrimeList.Primes.TakeWhile(p => p <= root).OrderBy(x => rand.Next()).ToArray();
+
+            for (ulong num = lo; num <= hi; ++num)
+            {
+                token.ThrowIfCancellationRequested();
+
+                if (IsPrime(num))
+                    ++count;
+            }
+
+            return count;
+
+            bool IsPrime(ulong num)
+            {
+                foreach (ulong x in primes)
+                {
+                    if (num % x == 0)
+                        return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Prime/Program.cs
@@ -0,0 +1,106 @@
+﻿using Prime;
+using System.Globalization;
+
+var range = GetRange();
+
+while (range.Lo > 0 && range.Hi > 0)
+{
+    try
+    {
+        var ctc = new CancellationTokenSource();
+
+        ctc.Cancel();
+
+        Task<int>[] tasks =
+        {
+            Task.Run(() => PrimeCount(range.Lo, range.Hi,ctc.Token),ctc.Token),
+            Task.Run(() => PrimeCount(range.Lo, range.Hi,ctc.Token),ctc.Token),
+            Task.Run(() => PrimeCount(range.Lo, range.Hi,ctc.Token),ctc.Token)
+        };
+
+        ReportTasks(tasks);
+
+        var finisher = await Task.WhenAny(tasks);
+
+        ReportTasks(tasks);
+
+        ctc.Cancel();
+
+        try
+        {
+            await Task.WhenAll(tasks);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        ReportTasks(tasks);
+
+        Console.WriteLine();
+        Console.WriteLine($"The calculation was done by algorithm number {Array.IndexOf(tasks, finisher)}");
+        Console.WriteLine($"There are {finisher.Result:N0} primes between {range.Lo:N0} and {range.Hi:N0}.");
+        Console.WriteLine();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    range = GetRange(); 
+}
+
+int PrimeCount(ulong lo, ulong hi, CancellationToken token)
+{
+    int count = 0;
+
+    var root = Math.Sqrt(hi);
+    var rand = new Random();
+
+    var primes = PrimeList.Primes.TakeWhile(p => p <= root).OrderBy(x => rand.Next()).ToArray();
+
+    for (ulong num = lo; num <= hi; ++num)
+    {
+        token.ThrowIfCancellationRequested();
+
+        if (IsPrime(num))
+            ++count;
+    }
+
+    return count;
+
+    bool IsPrime(ulong num)
+    {
+        foreach (ulong x in primes)
+        {
+            if (num % x == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
+
+(ulong Lo, ulong Hi) GetRange()
+{
+    Console.Write("Lower bound: ");
+    var lo = ulong.Parse(Console.ReadLine() ?? "", NumberStyles.AllowThousands);
+
+    Console.Write("Upper bound: ");
+    var hi = ulong.Parse(Console.ReadLine() ?? "", NumberStyles.AllowThousands);
+
+    return (lo, hi);
+}
+
+void ReportTasks(Task<int>[] tasks)
+{
+    for (int i = 0; i < tasks.Length; ++i)
+    {
+        Console.WriteLine($"Task: {i}");
+        Console.WriteLine($"IsCompleted: {tasks[i].IsCompleted}");
+        Console.WriteLine($"IsCompletedSuccessfully: {tasks[i].IsCompletedSuccessfully}");
+        Console.WriteLine($"IsCanceled: {tasks[i].IsCanceled}");
+        Console.WriteLine($"IsFaulted: {tasks[i].IsFaulted}");
+        Console.WriteLine();
+    }
+}
